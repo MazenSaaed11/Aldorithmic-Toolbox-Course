aec973b78bbe85322c9357cff082c1d4fbf1a25d
Mazen Saaed <80298280+MazenSaaed11@users.noreply.github.com>
2022-10-19 15:36:32 +0200

    Update 1Fibonacci_number.cpp

--- Week 2 Problems./Assignment./1Fibonacci_number.cpp
+++ Week 2 Problems./Assignment./1Fibonacci_number.cpp
@@ -3,16 +3,16 @@ using namespace std;
 #define Fifo ios_base::sync_with_stdio(false); cin.tie(NULL);
 int fib(int idx)
 {
-    if(idx <=1)
+    if (idx <= 1)
         return idx;
     else
     {
-        ll first = 0;
-        ll second = 1;
-        ll ans;
-        for(int i=2;i<=idx;i++)
+        int first = 0;
+        int second = 1;
+        int ans;
+        for (int i = 2; i <= idx; i++)
         {
-            ans = first+second;
+            ans = first + second;
             first = second;
             second = ans;
         }
@@ -22,7 +22,7 @@ int fib(int idx)
 int main() {
     Fifo
     int idx;
-    cin>>idx;
-    cout<<fib(idx);
+    cin >> idx;
+    cout << fib(idx);
     return 0;
 }
